644a6578234e5d130f94a5b5ea77cffe6b96ab5d
Nikola-jpg <84804348+Nikola-jpg@users.noreply.github.com>
2021-10-08 19:41:12 +0300

    "fisrt commit"

---
+++ src/main/java/com/example/serafan/SerafanApplication.java
@@ -0,0 +1,13 @@
+package com.example.serafan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerafanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerafanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/serafan/SerafanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerafanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
